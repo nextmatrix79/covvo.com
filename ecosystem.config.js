55851e2abebebbd9e24b06742fe13a6f685e31d6
Syed Suhaib Zia <suhaibzia786@gmail.com>
2025-04-16 22:19:54 +0400

    frontend updated

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Syed Suhaib Zia
+ * Created At: 4/16/25 10:17 PM
+ * Version: 1.0.0
+ */
+
+module.exports = {
+    apps: [
+        {
+            name: 'covvo.codencode.co',
+            script: 'npm',
+            args: 'run start',
+            watch: true,
+        },
+    ]
+};
